4df628b6f01dd7c6126a72e501524e36325489c4
Brian Hsu <a0938406813@gmail.com>
2025-06-16 17:38:38 +0800

    更改後端路徑/api

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -5,8 +5,10 @@ const instance = axios.create({
 })
 
 instance.interceptors.request.use(
+
   (config) => {
     const token = localStorage.getItem('token');
+    console.log(import.meta.env.VITE_API_BASE_URL);
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
